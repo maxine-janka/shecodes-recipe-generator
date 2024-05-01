1b5aa578a46f82405088531ceb2f5c6365eb3966
maxine-janka <159133549+maxine-janka@users.noreply.github.com>
2024-05-01 11:07:55 +1000

    Blink effect to "Generating recipe"

--- src/script.js
+++ src/script.js
@@ -15,8 +15,13 @@ function generateRecipe(event) {
   let recipeKeywords = document.querySelector("#user-input");
   let apiKey = "badact1734fb41b82663o4ab9a6e5204";
   let prompt = `Generate a recipe using ${recipeKeywords.value}`;
-  let context = "You are very knowledgable about home cooking and love to share easy home cooked recipes. Display one recipe per search. Strictly display the recipe in HTML format. Do not write the word HTML";
+  let context = "You are very knowledgable about home cooking and love to share easy home cooked recipes. Display only one recipe per search. Strictly display the recipe in HTML format. Do not write the word HTML";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let recipeElement = document.querySelector("#recipe-details");
+  recipeElement.classList.remove("hidden");
+  recipeElement.innerHTML = `<div class="blink">⏳ Generating a recipe with ${recipeKeywords.value} for you..</div> 
+`;
 
   console.log("Generating recipe");
   console.log(`Prompt: ${prompt}`);
