ae4d6c8c328950d440e86b19cf717f1c41e02e84
maxine-janka <159133549+maxine-janka@users.noreply.github.com>
2024-05-02 11:39:40 +1000

    Updates prompt
    Removed line for display images and subtitles (affecting CSS styling of headings)

--- src/script.js
+++ src/script.js
@@ -15,7 +15,7 @@ function generateRecipe(event) {
   let recipeKeywords = document.querySelector("#user-input");
   let apiKey = "badact1734fb41b82663o4ab9a6e5204";
   let prompt = `Generate a recipe using ${recipeKeywords.value}`;
-  let context = "You are very knowledgable about home cooking and love to share easy home cooked recipes. Display only one recipe per search. Do not display images or other subtitles. Strictly display the recipe in HTML format. Do not write the word HTML";
+  let context = "You are very knowledgable about home cooking and love to share easy home cooked recipes. Display only one recipe per search. Strictly display the recipe in HTML format. Do not write the word HTML";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   let recipeElement = document.querySelector("#recipe-details");
